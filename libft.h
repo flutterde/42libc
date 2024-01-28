ac24a886e2b441047290118b3675461e127d52f1
flutterde <71704747+flutterde@users.noreply.github.com>
2024-01-28 20:57:20 +0000

    printf Linked list added

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 14:30:48 by ochouati          #+#    #+#             */
-/*   Updated: 2024/01/27 16:37:38 by codespace        ###   ########.fr       */
+/*   Updated: 2024/01/28 20:54:43 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,5 +106,6 @@ void	ft_swap(int *xp, int *yp);
 void	ft_sort_int_tab(int *tab, int size);
 void	ft_free_strs(char **str);
 void	ft_print_string_bits(char *str);
+void	ft_lstprint_str(t_list *head);
 
 #endif
